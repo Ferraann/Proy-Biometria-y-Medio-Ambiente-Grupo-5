c3a851909d050b51768c493db056f97a4aadff7c
Caronte2 <147274913+Caronte2@users.noreply.github.com>
2025-10-28 11:40:55 +0100

    Separar código y preparar cosas de android

---
+++ src/android/app/src/main/java/com/example/pachacaj/myapplication/RegistrarseActivity.java
@@ -0,0 +1,12 @@
+package com.example.pachacaj.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrarseActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrarse);
+    }
+}
